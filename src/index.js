c24f5643d5e5e8c2269e1941ea62518d92c1a103
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-03-31 01:38:55 +0530

    Add DB connection

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
+import mongoose from 'mongoose'
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
+import { APP_PORT, IN_PROD, DB_URI } from './config'
 
-const { APP_PORT = 4000, NODE_ENV = 'development' } = process.env
+(async () => {
+  try {
+    await mongoose.connect(DB_URI, { useNewUrlParser: true })
 
-const IN_PROD = NODE_ENV === 'production'
+    const app = express()
 
-const app = express()
+    app.disable('x-powered-by')
 
-app.disable('x-powered-by')
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      playground: !IN_PROD
+    })
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  playground: !IN_PROD
-})
+    server.applyMiddleware({ app })
 
-server.applyMiddleware({ app })
-
-app.listen({ port: APP_PORT }, () =>
-  console.log(
-    `Server ready at http://localhost:${APP_PORT}${server.graphqlPath}`
-  )
-)
+    app.listen({ port: APP_PORT }, () =>
+      console.log(
+        `Server ready at http://localhost:${APP_PORT}${server.graphqlPath}`
+      )
+    )
+  } catch (err) {
+    console.error(err)
+  }
+})()
